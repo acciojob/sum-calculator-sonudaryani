cbe805f8ea9090a981d04176337d360e8bda0892
sonu7524 <sonudaryani135@gmail.com>
2023-07-21 21:28:16 +0530

    update now

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [sum, setSum] = useState(0);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setSum(sum + parseInt(event.target.value));
+}
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" onKeyUp={handleClick}/>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
